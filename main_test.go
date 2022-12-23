6a297f71f33adfda29b68c55391dd8b02ffab785
Venimir Petkov <venimirp@gmail.com>
2022-12-23 19:11:18 +0200

    test added

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -98,4 +99,56 @@ func TestSolution(t *testing.T) {
 			t.Errorf("Solution(%v, %v) = %v, expected %v", tc.T, tc.A, result, tc.expected)
 		}
 	}
+}
+
+func TestSolutionRandom(t *testing.T) {
+	// Set up a random number generator with a fixed seed
+	r := rand.New(rand.NewSource(0))
+
+	for i := 0; i < 1000; i++ {
+		// Generate a random value for N
+		N := r.Intn(100000) + 1
+
+		// Generate a random slice of skills for T
+		T := make([]int, N)
+		for j := 0; j < N; j++ {
+			T[j] = r.Intn(N)
+		}
+
+		// Generate a random slice of skills for A
+		M := r.Intn(N) + 1
+		A := make([]int, M)
+		for j := 0; j < M; j++ {
+			A[j] = r.Intn(N)
+		}
+
+		// Check if the Solution function is correct
+		result := Solution(T, A)
+		expected := expectedResult(T, A)
+		if result != expected {
+			t.Errorf("Solution(%v, %v) = %d, expected %d", T, A, result, expected)
+		}
+	}
+}
+
+// expectedResult calculates the expected result for the given input data
+func expectedResult(T, A []int) int {
+	// Set up a set to store the skills that have been learned
+	learned := map[int]bool{}
+
+	// Set up a variable to store the number of skills learned
+	skillsLearned := 0
+
+	// For each skill in array A:
+	for _, skill := range A {
+		// Traverse the skill tree from the skill to the root
+		for !learned[skill] {
+			learned[skill] = true
+			skill = T[skill]
+			skillsLearned++
+		}
+	}
+
+	// Return the number of skills learned
+	return skillsLearned
 }
